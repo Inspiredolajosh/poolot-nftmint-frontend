a997a145b9c08b607a9a44164191ef47d8bb069a
Inspiredolajosh <feranmiolawore@gmail.com>
2023-02-11 16:54:37 +0100

    Component update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Artworks from "./components/Artworks";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Loading from "./components/Loading";
+import Alert from "./components/Alert";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       </div>
         <Artworks />
         <Footer />
+        {/* <Alert /> */}
+        {/* <Loading /> */}
      </div>
   
     
